abdf39165506bc0a4f64f7c472ba7b587c1f4fde
Alyssa Choi <alyssachoi06@gmail.com>
2021-12-03 21:18:55 -0500

    Alyssa's initial commit

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,13 +8,26 @@ from django.db import models
 
 class Card(models.Model):
     # auto id generated
-    recto_or_verso = models.CharField(max_length=1)
-    deck = models.ForeignKey('Deck', null=True, on_delete=models.DO_NOTHING, )
+    deck = models.ForeignKey('Deck', null=True, on_delete=models.DO_NOTHING)
+    db_id = models.CharField(max_length=30, null=True)
+    recto_or_verso = models.CharField(max_length=1, null=True)
+    card = models.CharField(max_length=1, null=True)
+    suit = models.CharField(max_length=1, null=True)
+    title = models.CharField(max_length=200, null=True)
+    start_date = models.IntegerField(null=True)
+    end_date = models.IntegerField(null=True)
+    maker = models.CharField(max_length=30, null=True)
+    town = models.CharField(max_length=30, null=True)
+    type = models.CharField(max_length=30, null=True)
+    back_notes = models.CharField(max_length=30, null=True)
+    url = models.URLField(max_length=200, null=True)
 
 
 class Deck(models.Model):
     # auto id generated
-    name = models.CharField(max_length=300)
+    name = models.CharField(max_length=300, null=True)
+    period = models.CharField(max_length=1, null=True)
+    # period is either B, D, or A (before, during, after revolution)
 
     def __str__(self):
         # this just prints a helpful string in python console.
