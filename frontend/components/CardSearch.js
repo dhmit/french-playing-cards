c99cfd2c1ca6922da69a5d4d61cc13442747e682
Alyssa Choi <alyssachoi06@gmail.com>
2022-02-23 00:51:23 -0500

    Changes to CardSearch on 2/23 12:51 AM

--- frontend/components/CardSearch.js
+++ frontend/components/CardSearch.js
@@ -1,31 +1,30 @@
 import React from "react";
 import axios from "axios";
+import Select from "react-select";
 import {MultiSelect} from "react-multi-select-component";
 
 export default class CardSearch extends React.Component {
     state = {
         options: {
             periods: [{label: "Pre-Revolutionary"}, {label: "Revolutionary"},
-                {label: "Post-Revolutionary"}],
+                    {label: "Post-Revolutionary"}],
             cards: [{label: "Ace"}, {label: "King"}, {label: "Queen"}, {label: "Jack"}],
             suits: [{label: "Hearts"}, {label: "Clubs"}, {label: "Diamonds"}, {label: "Spades"}],
             rectoVerso: [{label: "Front"}, {label: "Back"}],
-            backNotes: [{label: "None"}, {label: "Library Card Catalogue"}, {
-                label: "Call and" +
-                    " Response"
-            },
-                {label: "Typographical Letters"}],
+            backNotes: [{label: "None"}, {label: "Library Card Catalogue"}, {label: "Call and" + " Response"},
+                        {label: "Typographical Letters"}],
             towns: [{label: "Unknown"}, {label: "Paris"}, {label: "Auvergne"}, {label: "Grenoble"},
-                {label: "Toulouse"}, {label: "Montauban"}, {label: "Lyon"}, {label: "Montpellier"},
-                {label: "Avignon"}],
+                    {label: "Toulouse"}, {label: "Montauban"}, {label: "Lyon"}, {label: "Montpellier"},
+                    {label: "Avignon"}],
             makers: [{label: "Unknown"}, {label: "Cordier"}, {label: "Della Bella"},
-                {label: "Antoine Reynaud"}, {label: "Vidal"}, {label: "Ressy"}, {label: "J. Minot"},
-                {label: "Lepautre"}, {label: "Bézu"}, {label: "Gayant"}, {label: "Dugourc"}, {label: "Pinaut"},
-                {label: "Gatteaux"}, {label: "J-L David"}, {label: "Isidore Patrois"}],
+                    {label: "Antoine Reynaud"}, {label: "Vidal"}, {label: "Ressy"}, {label: "J. Minot"},
+                    {label: "Lepautre"}, {label: "Bézu"}, {label: "Gayant"}, {label: "Dugourc"}, {label: "Pinaut"},
+                    {label: "Gatteaux"}, {label: "J-L David"}, {label: "Isidore Patrois"}]
         },
         periods: [],
         selectedItems: []
     }
+
     handleSearch = () => {
         console.log("clicked search!");
         /*let results = {
@@ -36,9 +35,10 @@ export default class CardSearch extends React.Component {
                 console.log("results from axios", results);
             });
     }
-    handleSelectChange = (selectedItems) => {
+
+    handleChange = (selectedItems) => {
         console.log(selectedItems);
-        this.setState({ selectedItems });
+        this.setState({selectedItems});
     }
 
     render() {
@@ -48,36 +48,71 @@ export default class CardSearch extends React.Component {
             <br/>
 
             <p>Time period</p>
-            <MultiSelect
-                id={"periods"}
-                selectedItems={this.state.selectedItems}
-                onChange={this.handleSelectChange}
-                options={this.state.options.periods}
+            <Select
+                isMulti
+                value = {this.state.selectedItems}
+                options = {this.state.options.periods}
+                onChange = {this.handleChange}
+
+                // id={"periods"}
+                // selectedItems={this.state.selectedItems}
+                // onChange={this.handleSelectChange}
+                // options={this.state.options.periods}
             />
             <br/>
 
             <p>Card</p>
-            <MultiSelect options={this.state.options.cards}/>
+            <Select
+                isMulti
+                value = {this.state.selectedItems}
+                options = {this.state.options.cards}
+                onChange = {this.handleChange}
+            />
             <br/>
 
             <p>Suit</p>
-            <MultiSelect options={this.state.options.suits}/>
+            <Select
+                isMulti
+                value = {this.state.selectedItems}
+                options = {this.state.options.suits}
+                onChange = {this.handleChange}
+            />
             <br/>
 
             <p>Front or back</p>
-            <MultiSelect options={this.state.options.rectoVerso}/>
+            <Select
+                isMulti
+                value = {this.state.selectedItems}
+                options = {this.state.options.rectoVerso}
+                onChange = {this.handleChange}
+            />
             <br/>
 
             <p>Back Notes</p>
-            <MultiSelect options={this.state.options.backNotes}/>
+            <Select
+                isMulti
+                value = {this.state.selectedItems}
+                options = {this.state.options.backNotes}
+                onChange = {this.handleChange}
+            />
             <br/>
 
             <p>Town</p>
-            <MultiSelect options={this.state.options.towns}/>
+            <Select
+                isMulti
+                value = {this.state.selectedItems}
+                options = {this.state.options.towns}
+                onChange = {this.handleChange}
+            />
             <br/>
 
             <p>Maker</p>
-            <MultiSelect options={this.state.options.makers}/>
+            <Select
+                isMulti
+                value = {this.state.selectedItems}
+                options = {this.state.options.makers}
+                onChange = {this.handleChange}
+            />
             <br/>
 
             <button onClick={this.handleSearch}>Search</button>
