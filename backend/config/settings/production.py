0890a72c82b24b983edc3131885b2e42366082e4
blms <ben@performantsoftware.com>
2022-05-13 16:21:05 +0000

    Prepare for deployment on Digital Ocean

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -9,10 +9,10 @@ from .base import *  # pylint: disable=unused-wildcard-import, wildcard-import
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  # set in venv activate
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  # set in venv activate
 
-ADMINS = ['rahmed@mit.edu']  # Django will email Ryaan on internal server errors
+# ADMINS = ['rahmed@mit.edu']  # Django will email Ryaan on internal server errors
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["45.55.60.82"]
 
-CORS_ORIGIN_WHITELIST = []
+CORS_ORIGIN_WHITELIST = ["http://45.55.60.82"]
