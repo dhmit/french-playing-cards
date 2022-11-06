0e40efe0a0bc9419afcf7bbb7fac87f937016746
Rhea Bhattacharjee <myrhea@gmail.com>
2022-11-06 17:28:52 -0500

    Moved search function to search page

--- frontend/components/pages/DatabaseSearch.js
+++ frontend/components/pages/DatabaseSearch.js
@@ -1,9 +1,12 @@
 import React from "react";
 import CardSearch from "../CardSearch";
+import Tabs from "../Tabs";
 
 const Search = () => {
     return (
-        <h2> Card search </h2>
+        <>
+            <CardSearch/>
+        </>
 
     );
 };
