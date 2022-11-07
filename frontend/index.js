509d82a31151b76a1a2f0f2c2df689e466fc03fc
Rhea Bhattacharjee <myrhea@gmail.com>
2022-11-07 18:55:42 -0500

    Better nav bar

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./scss/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Base from "./components/global/Base";
+import Nav from "./components/global/Nav";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Manufacture from "./components/pages/Manufacture";
@@ -16,58 +17,25 @@ import Search from "./components/pages/DatabaseSearch";
 import Games from "./components/pages/Games";
 import Bibliography from "./components/pages/Bibliography";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-        errorElement: <h2 style={{textAlign: 'center'}}>404: Page Not Found :(</h2>
-    },
-    {
-        path: "/about",
-        element: <About />
-    },
-    {
-        path: "/manufacture",
-        element: <Manufacture />
-    },
-    {
-        path: "/material-aspects",
-        element: <Material />
-    },
-    {
-        path: "/material-aspects/fronts",
-        element: <Fronts />
-    },
-    {
-        path: "/material-aspects/backs",
-        element: <Backs />
-    },
-    {
-        path: "/material-aspects/envelopes",
-        element: <Envelopes />
-    },
-    {
-        path: "/iconography",
-        element: <Iconography />
-    },
-    {
-        path: "/iconography/search",
-        element: <Search />
-    },
-    {
-        path: "/games",
-        element: <Games />
-    },
-    {
-        path: "/bibliography",
-        element: <Bibliography />
-    }
-  ]);
-
 ReactDOM.render(
     <div>
         <Base>
-            <RouterProvider router={router} />
+            <BrowserRouter>
+                <Nav />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/about" element={<About />} />
+                    <Route exact path="/manufacture" element={<Manufacture />} />
+                    <Route exact path="/material-aspects" element={<Material />} />
+                    <Route exact path="/material-aspects/fronts" element={<Fronts />} />
+                    <Route exact path="/material-aspects/backs" element={<Backs />} />
+                    <Route exact path="/material-aspects/envelopes" element={<Envelopes />} />
+                    <Route exact path="/iconography" element={<Iconography />} />
+                    <Route exact path="/iconography/search" element={<Search />} />
+                    <Route exact path="/games" element={<Games />} />
+                    <Route exact path="/bibliography" element={<Bibliography />} />
+                </Routes>
+            </BrowserRouter>
         </Base>
     </div>,
     document.getElementById("app_root")
