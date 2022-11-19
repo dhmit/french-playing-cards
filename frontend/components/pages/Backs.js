712b1d5aad6050b620ebec80299d58cf276290bf
Rhea Bhattacharjee <myrhea@gmail.com>
2022-11-19 14:44:24 -0500

    Created Backs page

--- frontend/components/pages/Backs.js
+++ frontend/components/pages/Backs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ImagePopup from "../ImagePopup";
 
 const Backs = () => {
 
@@ -7,8 +8,179 @@ const Backs = () => {
     }, []);
 
     return (
-        <h2 className="page-header"> Backs page </h2>
+    <>
+        <h2 className="page-header"> Backs </h2>
 
+        <div className="material-subpage-intro">
+            <img src={'/static/img/misc/mascot.jpg'}/>
+            <p className="material-subpage-blurb">
+                The backs of playing cards in France were left blank until the 1820s. Because paper was valuable, 
+                card players would not throw out their cards when they were too worn to be useful for gaming. Instead, 
+                people would use the backs of the cards for various purposes, some practical and some more fanciful. 
+                The examples below, drawn from the collections of the <i>Bibliothèque nationale de France</i>, capture some 
+                of these uses. To see the front of the card in question, please pass your cursor over it.
+            </p>
+            <img id="back-intro" src={'/static/img/materials/materialsBacks.jpeg'}/>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Library Card Catalogue</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                    In the decades before the French Revolution, bibliophiles realized that they could utilize used playing 
+                    cards to catalogue their book collections.  Surviving playing cards from the period suggest that this had 
+                    become the most frequent use for discarded playing cards by the end of the eighteenth century.
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/library-card-catalogue/Loudun.JPEG"/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Transcription: “Chap. 2892. Cruels effets de la vengeance du Cardinal de Richelieu ou Histoire des diables 
+                    de Loudun er d’Urbain Grandier. Amsterdam, Roger, 1716. In-12–veau"
+                    <br />
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-205 (18)
+                </p>
+            </div>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Legal Case Label</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                After card catalogues, the second most common usage for the backs of discarded playing cards was as filing 
+                labels for legal cases.
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/legal-case/legal-case.jpg" style={{height: "300px"}}/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Transcription: “Apointement en droit pour Claude des Chavanes, Sieur de Beauregard, contre Sieur Claude 
+                    Pesant Bourgeois de Villefranche. Mr. Jacques Bord. Dubost. Vermorel.”
+                    <br />
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, BOITE FOL-KH-383 (1, 14)
+                </p>
+            </div>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Money</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                    During the French Revolution, the government printed <i>billets de confiance</i>, or small denomination bills, 
+                    on the backs of used playing cards to supplement the printed bills known <i>assignats</i>.
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/money/money.JPEG" style={{height: "300px"}}/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Transcription: “Société patriotique, Ville de S. Maixent, Bon pour quinze sols. Billets de confiance 
+                    remboursables en assignats de deux cent livres et au sessous. No. 2895, 1791.”
+                    <br />
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (5, 145-147)
+                </p>
+            </div>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Meat Ration</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                    The military printed meat ration cards on the backs of used playing cards.
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/meat-ration/ration.JPEG" style={{height: "300px"}}/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Transcription: “Infanterie, Viande. Trois Rations. Controllé.”
+                    <br />
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (3, 105)
+                </p>
+            </div>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Business Cards</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                    Merchants in Paris and other cities would print their business cards on the back of used playing cards.
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/business-cards/Business.JPEG" style={{height: "300px"}}/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Transcription: “Bienvenu Samson, Négociant au Bourget, près Paris, ci-devant à St.-Denis. Tient magasin 
+                    de Vons, Eaux-de-vie, Epicerie.”
+                    <br />
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (7, 210).
+                </p>
+            </div>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Flash Cards</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                    The backs of cards were used to teach spelling and grammar
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/flash-cards/flash card.jpg" style={{height: "300px"}}/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Transcription:  “avoir infinitive – present”
+                    <br />
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, BOITE FOL-KH-383 (1, 16).
+                </p>
+            </div>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Call and Response</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                    The backs of used playing cards might be repurposed for provocative parlor games.
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/call-and-response/89-99 Bezu.JPEG" style={{height: "300px"}}/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Transcription: “Etes-vous séduisant?”
+                    <br />
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (5, 153)
+                </p>
+            </div>
+        </div>
+
+        <div className='material-subpage-item'>
+        <h3>Pastoral Drawing</h3>
+            <div className='material-subpage-item-contents'>
+                <p>
+                    An unknown artist used the back of this Jack of Clubs to sketch a pastoral scene.
+                </p>
+                <div className='material-subpage-item-images'>
+                    <div className='material-subpage-item-image-caption'>
+                        <img src="/static/img/materials/backs/pastoral-drawing/Pastoral Drawing.jpg" style={{height: "300px"}}/>
+                    </div>
+                </div>
+                <p className="back-caption">
+                    Source: Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-205 (18)
+                </p>
+            </div>
+        </div>
+    </>
     );
 };
 
--- frontend/components/pages/Fronts.js
+++ frontend/components/pages/Fronts.js
@@ -11,28 +11,28 @@ const Fronts = () => {
     <>
         <h2 className="page-header"> Fronts: The Queen of Hearts </h2>
 
-        <div id='FrontsIntro'>
-        <img src={'/static/img/misc/mascot.jpg'}/>
-        <p id='FrontsBlurb'>
-            Most French playing cards before the nineteenth century were manufactured using stencils. 
-            The stencil process for point cards (ace through ten) was simple, involving only one 
-            color and a design that was standardized across the kingdom. Designs for face cards, 
-            however (the king, queen, and jack), varied widely, and utilized four or five different 
-            colors. Furthermore, for purposes of control and taxation, the state before 1789 and 
-            after 1797 demanded that manufacturers adhere to a single authorized design. 
-            <br/>
-            <br/>
-            An analysis of the variations in face card design from the Old Regime through the 
-            Revolution of 1848 reveals substantial variation across time that can be explained by 
-            changing political, economic, and cultural factors. Here we focus on the Queen of Hearts, 
-            but those interested in exploring changes in the iconography of other French face cards 
-            in this period should explore our face card iconography search tool.
-        </p>
+        <div className="material-subpage-intro">
+            <img src={'/static/img/misc/mascot.jpg'}/>
+            <p className="material-subpage-blurb">
+                Most French playing cards before the nineteenth century were manufactured using stencils. 
+                The stencil process for point cards (ace through ten) was simple, involving only one 
+                color and a design that was standardized across the kingdom. Designs for face cards, 
+                however (the king, queen, and jack), varied widely, and utilized four or five different 
+                colors. Furthermore, for purposes of control and taxation, the state before 1789 and 
+                after 1797 demanded that manufacturers adhere to a single authorized design. 
+                <br/>
+                <br/>
+                An analysis of the variations in face card design from the Old Regime through the 
+                Revolution of 1848 reveals substantial variation across time that can be explained by 
+                changing political, economic, and cultural factors. Here we focus on the Queen of Hearts, 
+                but those interested in exploring changes in the iconography of other French face cards 
+                in this period should explore our face card iconography search tool.
+            </p>
         </div>
 
-        <div className='FrontsItem'>
+        <div className='material-subpage-item'>
         <h3>The Old Regime (to 1789)</h3>
-            <div className='FrontsItemContents'>
+            <div className='material-subpage-item-contents'>
                 <p>
                 By 1750, the crown had divided the kingdom into nine regions for purposes of collecting excise 
                 taxes on the manufacture of playing cards. Each district had a unique face card iconography
@@ -41,20 +41,20 @@ const Fronts = () => {
                 not paid the excise tax. Below you see official examples of the Queen of Hearts from the Paris, 
                 Guyenne, and Dauphiné regions.
                 </p>
-                <div className='FrontsItemImages'>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-images'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src='/static/img/materials/fronts/old-regime/paris.jpeg'/>
                     <p>
                         Paris. Source: Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-205 (7)
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/old-regime/guyenne.jpeg'}/>
                     <p>
                     Guyenne.  Source: Bibliothèque nationale de France, Cabinet des Estampes, BOITE FOL-KH-383 (3, 82)                    
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/old-regime/dauphine.jpeg'}/>
                     <p>
                     Dauphiné.  Source: Bibliothèque nationale de France, Cabinet des Estampes, BOITE FOL-KH-383 (2, 40)    
@@ -65,9 +65,9 @@ const Fronts = () => {
             </div>
         </div>
 
-        <div className='FrontsItem'>
+        <div className='material-subpage-item'>
         <h3>The French Revolution (1789-1799)</h3>
-            <div className='FrontsItemContents'>
+            <div className='material-subpage-item-contents'>
                 <p>
                     In the early years of the Revolution, the legislative assemblies abolished the 
                     guilds that had controlled the card-making industry and abolished the excise tax 
@@ -94,21 +94,21 @@ const Fronts = () => {
                     antiquity, in her left hand. 
                 </p>
 
-                <div className='FrontsItemImages'>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-images'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/revolution/Jaume.Rev.1.JPEG'}/>
                     <p>
                     Jaume and Dugourc original design.  “Freedom of Religion.” Source: Bibliothèque nationale de France, RESERVE BOITE ECU-KH-204 (6).
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/revolution/Sigogne.Rev.JPEG'}/>
 
                     <p>
                     Jaume and Dugourc variant.  “Freedom of the Press.” Source:  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (5, 154).                    
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/revolution/Jaume.Rustic.JPEG'}/>
                     <p>
                     Old Regime “Liberty” modification. Source:  Bibliothèque nationale de France, Cabinet des Estampes, BOITE FOL-KH-383 (4, 102).    
@@ -130,20 +130,20 @@ const Fronts = () => {
                     right are variations on the season of summer. 
                 </p>
 
-                <div className='FrontsItemImages'>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-images'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/svb-esc-bezu/SVB.1.JPEG'}/>
                     <p>
                     SVB.  Source:  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-204 (1).
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/svb-esc-bezu/ESC.1.JPEG'}/>
                     <p>
                     ESC.  Source:  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (5BIS, 164).                    
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/svb-esc-bezu/Bezu.1.jpg'}/>
                     <p>
                     Bézu. Source:  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE KH-204-BOITE FOL.    
@@ -154,9 +154,9 @@ const Fronts = () => {
             </div>
         </div>
 
-        <div className='FrontsItem'>
+        <div className='material-subpage-item'>
         <h3>Napoleon (1799-1815)</h3>
-            <div className='FrontsItemContents'>
+            <div className='material-subpage-item-contents'>
                 <p>
                 By the time Napoleon came to power, the French state had resumed the excise tax on the manufacture of playing cards.  
                 In addition, Napoleon's officials decided to create one uniform face card design for the entire nation.  
@@ -167,20 +167,20 @@ const Fronts = () => {
                 therefore more popular.  In 1815, a designer named Gustave-Armand Houbigant patented a new iconography with a more 
                 romantic, medieval look.
                 </p>
-                <div className='FrontsItemImages'>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-images'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/napoleon/David.JPEG'}/>
                     <p>
                         David.  Source:  Bibliothèque nationale de France, Cabinet des Estampes, PET FOL-KH-34 (C, 28).
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/napoleon/Gatteaux.JPEG'}/>
                     <p>
                         Gatteaux.  Source:  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (7BIS, 326).                    
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/napoleon/Houbigant.jpg'}/>
                     <p>
                     Houbigant.  Source:  British Library, 1896,0501.1292.1-12.    
@@ -191,28 +191,28 @@ const Fronts = () => {
             </div>
         </div>
 
-        <div className='FrontsItem'>
+        <div className='material-subpage-item'>
         <h3>Revolution and the Occult in the Nineteenth Century (1815-1848)</h3>
-            <div className='FrontsItemContents'>
+            <div className='material-subpage-item-contents'>
                 <p>
                 After the fall of Napoleon in 1815, French politics veered between monarchy and republicanism.  
                 Decks of cards designed in the revolutionary years of 1830 and 1848 expressed the hope for change, 
                 while other decks of cards tapped into occult tendencies in the early nineteenth century.
                 </p>
-                <div className='FrontsItemImages'>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-images'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/1815-1848/1830.Barricades.JPEG'}/>
                     <p>
                         The Revolution of 1830.  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE BOITE ECU-KH-167 (10, 399-400).
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
-                    <ImagePopup src={'/static/img/materials/fronts/1815-1848/1830.Barricades.JPEG'}/>
+                <div className='material-subpage-item-image-caption'>
+                    <ImagePopup src={'/static/img/materials/fronts/1815-1848/1848.JPEG'}/>
                     <p>
                         The Revolution of 1848.  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE KH-34 (A, 7)-BOITE ECU.                    
                     </p>
                 </div>
-                <div className='FrontsItemImageCaption'>
+                <div className='material-subpage-item-image-caption'>
                     <ImagePopup src={'/static/img/materials/fronts/1815-1848/Cartomancy.JPEG'}/>
                     <p>
                     Cartomancy, between 1814 and 1830.  Bibliothèque nationale de France, Cabinet des Estampes, RESERVE KH-34 (3, 29)-BOITE ECU.    
