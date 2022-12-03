c21d26badfd1609eac97c223f1d9928cb37bf702
Rhea Bhattacharjee <myrhea@gmail.com>
2022-12-02 22:09:35 -0500

    Created Bibliography page

--- frontend/components/pages/Bibliography.js
+++ frontend/components/pages/Bibliography.js
@@ -7,8 +7,106 @@ const Bibliography = () => {
     }, []);
 
     return (
-        <h2 className="page-header"> Bibliography page </h2>
+        <>
+        <h2 className="page-header"> Bibliography </h2>
 
+        <img id="bibliography-mascot" src="/static/img/misc/bibliography-image.jpeg"/>
+
+        <div className="bibliography-content">
+            <h3> I. Collections of Historical Playing Cards with Substantial French Holdings </h3>
+            <p>
+                <i>Bibliothèque nationale de France</i>.  The BnF has digitized more than 1800 items 
+                in its playing card collections in the Cabinet des estampes.  To browse these holdings, 
+                go to <a href="https://gallica.bnf.fr">https://gallica.bnf.fr</a>.  Select “images” in 
+                the dropdown menu labeled “Tout Gallica,” then enter “JeuCart” in the search bar.  Users 
+                can filter the results by author, century, and language.
+            </p>
+            <p>
+                <i>Musée français de la carte à jouer</i>.  This museum, located in the Parisian suburb of 
+                Issy-les-Moulineaux, contains exhibits about the history of French playing cards as well as 
+                collections of cards and a library devoted to their history.  The Museum’s web site is 
+                available <a href="https://www.museecarteajouer.com/">here</a>.
+            </p>
+            <p>
+                <i>Columbia University, Rare Book and Manuscript Library</i>.  Columbia conserves the Albert 
+                Field Collection of Playing Cards, with more than 6300 individual decks.  A description and 
+                access to a database can be found at <a href="https://dlc.library.columbia.edu/field_playing_cards">
+                https://dlc.library.columbia.edu/field_playing_cards</a>.
+            </p>
+            <p>
+                <i>Yale University, Beinecke Library.</i> The Beinecke holds the Cary Card Collection, consisting 
+                of over 1000 whole or partial decks of cards and many other playing card-related items.  For a 
+                description of the collection, and a link to searchable database, go to <a href="https://beinecke.library.yale.edu/collections/highlights/cary-playing-card-database">
+                https://beinecke.library.yale.edu/collections/highlights/cary-playing-card-database</a>. There is 
+                a useful essay describing the collection <a href="https://carycards.beinecke.library.yale.edu/CaryEssaysWeb.htm">
+                here</a>.
+            </p>
+        </div>
+
+        <div className="bibliography-content">
+            <h3>II. Selected Scholarship on the History of French Playing Cards and Gaming</h3>
+            <p> 
+                Allemagne, Henri-René d’. <i>Les Cartes à jouer du XIVe au XXe siècle</i>, 2 vols., Paris, 1906.
+            </p>
+            <p> 
+                Belmas, Elisabeth.  <i>Jouer autrefois. Essai sur le jeu dans la France moderne, XVIe-XVIIIe siècle</i> 
+                Seyssel: Champ Vallon, 2006.
+            </p>
+            <p> 
+                Bert, Jean-François. <i>Comment pense un savant? Un physicien des Lumières et ses cartes à jouer</i>, 
+                Paris: Anamosa, 2018.
+            </p>
+            <p> 
+                Depaulis, Thierry. <i>Les Cartes de la Révolution: Cartes à jouer et propogande.</i> Issy-les-Moulineaux: 
+                Musée français de la Carte à jouer, 1989.
+            </p>
+            <p> 
+                Depaulis, Thierry. “Des ‘figures maussades & révoltantes’ : Diderot et les cartes à jouer,” <i>Le Vieux 
+                Papier</i>, fasc. 412 (April 2014), pp. 256-64; fasc. 413 (July 2014), pp. 289-98; fasc. 414 (October 2014), 
+                pp. 342-53; and fasc. 415 (January 2015), pp. 409-21.
+            </p>
+            <p> 
+                Desbarats, Catherine.  “On Being Surprised: by New France’s Card Money, for Example,” <i>Canadian Historical 
+                Review 102-1</i> (March 2021), 125-51.
+            </p>
+            <p> 
+                Dunkley, John. <i>Gambling: A Social and Moral Problem in France</i>, 1685-1792, Oxford: The Voltaire Foundation, 
+                1985. 
+            </p>
+            <p>
+                Freundlich, Francis. <i>Le Monde du jeu à Paris</i>, 1715-1800, Paris : Albin Michel, 1995.
+            </p>
+            <p>
+                Kavanaugh, <i>Thomas M. Enlightenment and the Shadows of Chance: The Novel and the Culture of Gambling in 
+                Eighteenth-Century France</i>, Baltimore and London: The Johns Hopkins University Press, 1993.
+            </p>
+            <p>
+                Keller, William B. <i>A Catalogue of the Cary Collection of Playing Cards in the Yale University Library.</i> 
+                New Haven: The Library, 1981.
+            </p>
+            <p>
+                Netchine, Ève, ed., <i>Jeux de princes, jeux de vilains</i>, Paris: Bibliothèque nationale de France / Seuil, 2009.
+            </p>
+            <p>
+                Ravel, Jeffrey. “On the Playing Cards of the Dulac Brothers in the Year II,” <i>Studies in Eighteenth-Century 
+                Culture</i>, Vol 52, forthcoming March 2023.
+            </p>
+            <p>
+                Ravel, Jeffrey.  “Accommodation: The Policing of Used Playing Cards in Paris in the 1780s,” Pascal Bastien, 
+                ed. <i>Paris, Policing, and Urban Sociability in Eighteenth-Century Paris</i>, Oxford University Studies in the 
+                Enlightenment, forthcoming.
+            </p>
+            <p>
+                Ravel, Jeffrey.  “Plus de rois, de dames, de valets”: Playing Cards During the French Revolution,” <i>Oxford 
+                University Studies in the Enlightenment</i>, forthcoming.
+            </p>
+            <p>
+                Spies-Gann, Paris Amanda.  “A Princely Education through Print: Stefano della Bella’s 1644 Jeux de Cartes Etched 
+                for Louis XIV” <i>Getty Research Journal</i> no. 9 (2017): pp. 1-22.
+            </p>
+        </div>
+
+        </>
     );
 };
 
