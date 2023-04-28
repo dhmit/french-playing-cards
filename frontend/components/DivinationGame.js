213cdaa91746103458289461c642578f7fc5dc3f
Rhea Bhattacharjee <33497811+rhea-bhattacharjee@users.noreply.github.com>
2023-04-28 15:29:17 -0400

    get keywords from cards

--- frontend/components/DivinationGame.js
+++ frontend/components/DivinationGame.js
@@ -6,9 +6,11 @@ const NUM_CARDS = 33;
 
 const DivinationGame = () => {
     const [cards, setCards] = React.useState([]);
+    const [reading, setReading] = React.useState("");
 
-    function getCards(num) {
+    async function getCards(num) {
         let cardStrings = new Set();
+        setCards([]);
 
         while(cardStrings.size < num) {
             let cardNum = Math.floor(Math.random() * (NUM_CARDS)) + 1;
@@ -17,40 +19,103 @@ const DivinationGame = () => {
             cardStrings.add(cardNum + "." + cardUp);
         }
 
-        console.log(cardStrings);
+        let chosen = [];
 
-        setCards([]);
-
-        for(const card of cardStrings) {
-            let [cardNum, cardUp] = card.split(".");
+        for(const cardString of cardStrings) {
+            let [cardNum, cardUp] = cardString.split(".");
             cardNum = parseInt(cardNum);
             cardUp = parseInt(cardUp);
             
-            axios.get('/divination-card/', {
+            let response = await axios.get('/divination-card/', {
                 params: {
                     number: cardNum,
                     orientation: cardUp,
                     language: i18next.language
                 }
-              })
-              .then(function (response) {
-                console.log(response.data);
-                setCards(current => [...current, response.data.card]);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
+            });
+              
+            chosen.push(response.data.card);
         }
+
+        setCards(chosen);
+        return chosen;
     };
+
+    function getKeywords(cards) {
+        let keywords = new Set();
+
+        // check multiples
+        for(let i = 0; i < cards.length; i++) {
+            let card = cards[i].card;
+            let up = cards[i].orientation;
+            let matches = [card];
+
+            for(let j = 0; j < cards.length; j++) {
+                if(i !== j && card === cards[j].card && up === cards[j].orientation) {
+                    matches.push(cards[j].card);
+                }
+            }
+
+            let repeats = matches.length;
+            if (repeats === 4) keywords.add(cards[i].quad);
+            if (repeats === 3) keywords.add(cards[i].triple);
+            if (repeats === 2) keywords.add(cards[i].double);
+        }
+
+        // check pairs
+        for(let i = 0; i < cards.length; i++) {
+            let num = cards[i].number;
+            for(let j = i; j < cards.length; j++) {
+                if (num + cards[j].number === 31) {
+                    keywords.add(cards[i].pair);
+                    keywords.add(cards[j].pair);
+                    break;
+                }
+            }
+        }
+
+        // choose keyword: 1. check eteilla 2. choose title/subtitle
+        for (let i = cards.length - 1; i >= 0; i--) {
+            if(i !== cards.length - 1 && cards[i+1].card === 'E') {
+                keywords.add(cards[i].etteilla);
+            }
+            else if (cards[i].number !== 1) {
+                let pick_title = Math.round(Math.random()); // randomly pick title or subtitles
+                if(pick_title) {
+                    keywords.add(cards[i].title);
+                }
+                else {
+                    keywords.add(cards[i].subtitle);
+                }
+            }
+        }
+
+        return keywords;
+    }
+
+    async function divinate(num) {
+        const chosen = await getCards(num);
+        const keywords = getKeywords(chosen);
+        const divination_string = "Your keywords are: " + [...keywords].join(', ');
+
+        // get reading from keywords
+
+        setReading(divination_string);
+    }
 
     return (
         <>
-            <button onClick={() => getCards(3)}>Get 3 cards</button>
+            <button onClick={() => divinate(3)}>Get 3 cards</button>
+            <button onClick={() => divinate(5)}>Get 5 cards</button>
+            <br />
             <div id="divination-cards">
                 {cards.map(card => (
                     <img key={card.id} src={"/static/img/cartomancy/" + card.image} />
                 ))}
             </div>
+            <p>
+                {reading}
+            </p>
         </>
     );
 };
