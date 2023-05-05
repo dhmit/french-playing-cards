55d421a0833852f967f81721c35e7339d604ef4c
pauladc <pauladc@mit.edu>
2023-05-05 15:21:52 -0400

    added openai starter code

--- frontend/components/DivinationGame.js
+++ frontend/components/DivinationGame.js
@@ -1,7 +1,12 @@
 import React from "react";
 import axios from "axios";
 import i18next from "i18next";
+import { Configuration, OpenAIApi } from "openai";
 
+const configuration = new Configuration({
+    apiKey: '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',
+    });
+const openai = new OpenAIApi(configuration);
 const NUM_CARDS = 33;
 
 const DivinationGame = () => {
@@ -96,11 +101,104 @@ const DivinationGame = () => {
     async function divinate(num) {
         const chosen = await getCards(num);
         const keywords = getKeywords(chosen);
-        const divination_string = "Your keywords are: " + [...keywords].join(', ');
+        const divination_string = [...keywords].join(', ');
 
         // get reading from keywords
 
         setReading(divination_string);
+
+    }
+
+    async function generate(req, res) {
+
+    if (!configuration.apiKey) {
+        res.status(500).json({
+        error: {
+            message: "OpenAI API key not configured, please follow instructions in README.md",
+        }
+        });
+        return;
+    }
+
+    const predictionQuestion = req.body.prediction || '';
+    if (predictionQuestion.trim().length === 0) {
+        res.status(400).json({
+        error: {
+            message: "Please enter a valid question",
+        }
+        });
+        return;
+    }
+
+    const keywords = req.body.keywords || '';
+    if (keywords.trim().length === 0) {
+        res.status(400).json({
+        error: {
+            message: "Please click on +1 cards to obtain keywords",
+        }
+        });
+        return;
+    }
+
+
+    try {
+        const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: generatePrompt(predictionQuestion, keywords),
+        temperature: 0.6,
+        });
+        res.status(200).json({ result: completion.data.choices[0].text });
+    } catch(error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+        console.error(error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+        } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+        res.status(500).json({
+            error: {
+            message: 'An error occurred during your request.',
+                }
+            });
+            }
+        }
+    }
+
+    function generatePrompt(predictionQuestion, keywords) {
+    return `Write a prediction in response to the question: ${predictionQuestion}, using the following words:${keywords}`;
+    }
+
+
+
+    // api part
+    const [predictionInput, setPredictionInput] = React.useState("");
+
+    const [result, setResult] = React.useState();
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        try {
+        const response = await fetch(generate(), {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prediction: predictionInput, keywords: reading }),
+        });
+
+        const data = await response.json();
+        if (response.status !== 200) {
+            throw data.error || new Error(`Request failed with status ${response.status}`);
+        }
+
+        console.log(data.result);
+        setResult(data.result);
+        setPredictionInput("");
+        } catch(error) {
+        // Consider implementing your own error handling logic here
+        console.error(error);
+        alert(error.message);
+        }
     }
 
     return (
@@ -112,10 +210,22 @@ const DivinationGame = () => {
                 {cards.map(card => (
                     <img key={card.id} src={"/static/img/cartomancy/" + card.image} />
                 ))}
+                <h3>Input a question to generate a tarot reading</h3>
+                <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    name="question"
+                    placeholder="Enter a question"
+                    value={predictionInput}
+                    onChange={(e) => setPredictionInput(e.target.value)}
+                />
+                {/* <input type="submit" value="Generate predictions" /> */}
+                </form>
             </div>
             <p>
                 {reading}
             </p>
+            <p>{result}</p>
         </>
     );
 };
