83e6330d11e57112d8415e94220627b2e0a56272
Rhea Bhattacharjee <33497811+rhea-bhattacharjee@users.noreply.github.com>
2023-05-05 15:19:57 -0400

    question input

--- frontend/components/DivinationGame.js
+++ frontend/components/DivinationGame.js
@@ -5,6 +5,7 @@ import i18next from "i18next";
 const NUM_CARDS = 33;
 
 const DivinationGame = () => {
+    const [question, setQuestion] = React.useState("");
     const [cards, setCards] = React.useState([]);
     const [reading, setReading] = React.useState("");
 
@@ -97,16 +98,19 @@ const DivinationGame = () => {
         const chosen = await getCards(num);
         const keywords = getKeywords(chosen);
         const divination_string = "Your keywords are: " + [...keywords].join(', ');
+        const input_string = "Write a prediction in response to the question \"" + question + "\" using the following words: " + [...keywords].join(', ');
 
         // get reading from keywords
+        //let reading = await()
 
         setReading(divination_string);
     }
 
     return (
         <>
-            <button onClick={() => divinate(3)}>Get 3 cards</button>
-            <button onClick={() => divinate(5)}>Get 5 cards</button>
+            <input type="text" placeholder="Ask a question..." onChange={(e) => setQuestion(e.target.value)}/>
+            <button type="submit" onClick={() => divinate(3)}>Get 3 cards</button>
+            <button type="submit" onClick={() => divinate(5)}>Get 5 cards</button>
             <br />
             <div id="divination-cards">
                 {cards.map(card => (
